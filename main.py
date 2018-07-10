6a8697466f5de1d64e1235be9e60e174e0f3426a
shuoGG <328893769@qq.com>
2018-07-10 17:32:34 +0800

    use CandyUi

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 import KeyActionWidget
-from qss_ui_theme import qss_setting
-from qss_ui_theme import green_theme
-from qss_ui_theme import window_titlebar
-
+from QCandyUi import CandyWindow
 
 def run_with_titlebar():
     app = QApplication(sys.argv)
     keywidget = KeyActionWidget.KeyActionWidget()
-    mainWindow = window_titlebar.WindowWithTitleBar(keywidget, qss_setting.DARKBLUEGREEN, 0)
+    mainWindow = CandyWindow.createWindow(keywidget, 'blue')
     mainWindow.setWindowTitle('Key action')
-    mainWindow.setWindowIcon(QIcon(window_titlebar.imageroot + 'myicon.ico'))
-    green_theme.setAppGreenStyle()
+    mainWindow.setWindowIcon(QIcon('myicon.ico'))
     mainWindow.show()
     sys.exit(app.exec_())
 
